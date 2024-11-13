ad1b65245ed9c0835e7a83b3d3131363f7e7c9df
Otavio Sousa <otaviosousamartins14@gmail.com>
2024-11-13 16:08:49 -0300

    add category.dart

---
+++ lib/models/category.dart
@@ -0,0 +1,27 @@
+import 'package:app_recipes/widgets/category_grid_item.dart';
+import 'package:app_recipes/data/dummy_data.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget{
+  const CategoriesScreen ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Pick your category'),
+        ),
+        body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3/2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: [
+            //availableCategories.map((e) => CategoryGridItem(category: category)).toList()
+            for (final category in availableCategories)
+              CategoryGridItem(category: category)
+          ],)
+    );
+  }
+}
