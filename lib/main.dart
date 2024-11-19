0b9a8eb1b043a20236dddfb6cff50c1b423dc638
Otavio Sousa <otaviosousamartins14@gmail.com>
2024-11-18 21:58:28 -0300

    final da pt 1

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,27 @@ import 'package:google_fonts/google_fonts.dart';
 //Link para o google_fonts: https://pub.dev/packages/google_fonts
 //Você pode adicionar ao projeto com o comando: flutter pub add google_fonts
 
-
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
-      brightness: Brightness.dark,
-      seedColor: const Color.fromARGB(255, 131, 57, 0),
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
   ),
   textTheme: GoogleFonts.latoTextTheme(),
 );
 
-void main(){
+void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      //home: const CategoriesScreen(),
+      home: const CategoriesScreen(),
     );
   }
 }
--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,27 +1,10 @@
-import 'package:app_recipes/widgets/category_grid_item.dart';
-import 'package:app_recipes/data/dummy_data.dart';
 import 'package:flutter/material.dart';
 
-class CategoriesScreen extends StatelessWidget{
-  const CategoriesScreen ({super.key});
+class Category {
+  const Category(
+      {required this.id, required this.title, this.color = Colors.orange});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Pick your category'),
-        ),
-        body: GridView(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3/2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          children: [
-            //availableCategories.map((e) => CategoryGridItem(category: category)).toList()
-            for (final category in availableCategories)
-              CategoryGridItem(category: category)
-          ],)
-    );
-  }
+  final String id;
+  final String title;
+  final Color color;
 }
